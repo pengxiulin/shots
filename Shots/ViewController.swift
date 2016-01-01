9395bfe8067e90f2f658e5812e3118b1abe6e5d2
pengxiulin <pengxiulin@live.com>
2016-01-01 13:52:59 +0800

    Initial Commit

---
+++ Shots/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shots
+//
+//  Created by 彭修林 on 1/1/16.
+//  Copyright © 2016 彭修林. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
